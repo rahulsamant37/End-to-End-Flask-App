ee0cfdf02bc966dab765502cbf459804b7d902f7
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-19 02:12:47 +0530

    intilize-data-transformation

--- src/datascience/pipeline/data_tranformation_pipeline.py
+++ src/datascience/pipeline/data_tranformation_pipeline.py
@@ -1,6 +1,7 @@
 from src.datascience.config.configuration import ConfigurationManager
 from src.datascience.components.data_transformation import DataTransformation
 from src.datascience import logger
+from pathlib import Path
 
 STAGE_NAME="Data Transformation Stage"
 
@@ -9,10 +10,18 @@ class DataTransformationTrainingPipeline:
         pass
 
     def initiate_data_ingestion(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_spliting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"),'r') as f:
+                status=f.read().split(" ")[-1]
+            if status == True:
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+            else:
+                raise Exception("Your data scheme is not valid")
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     try:
